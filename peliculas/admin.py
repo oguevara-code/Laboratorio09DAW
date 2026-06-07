b00bfd9d9dd8662b957b5ea767d7ddb9ea63a51d
oguevara-code <oguevara@unsa.edu.pe>
2026-06-07 18:07:23 -0500

    Implementación de relaciones uno a muchos y muchos a muchos

--- peliculas/admin.py
+++ peliculas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Language)
+admin.site.register(Framework)
+admin.site.register(Movie)
+admin.site.register(Character)
--- peliculas/models.py
+++ peliculas/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Uno a muchos (One To Many)
+
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Framework(models.Model):
+    name = models.CharField(max_length=50)
+    language = models.ForeignKey(
+        Language,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+    
+
+# Muchos a muchos (Many To Many)
+
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
